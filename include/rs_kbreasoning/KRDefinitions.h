5d41b7cb571f685c4d90dcc1e682683c062d0512
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2015-12-15 14:30:00 +0100

    header file to hold name mappings between RS and Knowrob

---
+++ include/rs_kbreasoning/KRDefinitions.h
@@ -0,0 +1,26 @@
+#ifndef KRDEFINITIONS_H
+#define KRDEFINITIONS_H
+
+#endif // KRDEFINITIONS_H
+
+
+#include <map>
+#include <string>
+
+namespace rs_kbreasoning {
+
+  std::map<std::string,std::string> krNameMapping = {
+    {"icetea","rs_test_objects:'PfannerIceTea'"},
+    {"mondamin","rs_test_objects:'MondaminPancakeMix'"},
+    {"cereal","rs_test_objects:'KellogsCornFlakes'"},
+    {"plate","rs_test_objects:'Plate'"},
+    {"pancake_maker","rs_test_objects:'PancakeMaker'"},
+    {"spatula","rs_test_objects:'Spatula'"},
+    {"pitcher","rs_test_objects:'Pitcher'"},
+    {"milk","rs_test_objects:'Milk'"},
+    {"cup","rs_test_objects:'Cup'"}
+  };
+
+}
+
+//objects

355a483f8d15e02956770f957eed4e1d9efaa281
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2016-06-22 13:19:30 +0200

    updated KR mappings

--- include/rs_kbreasoning/KRDefinitions.h
+++ include/rs_kbreasoning/KRDefinitions.h
@@ -10,6 +10,14 @@
 namespace rs_kbreasoning {
 
   std::map<std::string,std::string> krNameMapping = {
+    //superclasses
+    {"DRINK","knowrob:'Drink'"},
+    {"FOODORDRINKORINGREDIENT","knowrob:'FoodOrDrinkOrIngredient'"},
+    {"CONTAINER","knowrob:'Container'"},
+    {"COOKING UTENSIL,knowrob:'CookingUtensil'"},
+    {"ELECTRICAL DEVICE","knowrob:'ElectricalDevice'"},
+
+    //objects
     {"icetea","rs_test_objects:'PfannerIceTea'"},
     {"mondamin","rs_test_objects:'MondaminPancakeMix'"},
     {"cereal","rs_test_objects:'KellogsCornFlakes'"},
@@ -18,9 +26,14 @@ namespace rs_kbreasoning {
     {"spatula","rs_test_objects:'Spatula'"},
     {"pitcher","rs_test_objects:'Pitcher'"},
     {"milk","rs_test_objects:'Milk'"},
-    {"cup","rs_test_objects:'Cup'"}
+    {"cup","rs_test_objects:'Cup'"},
+    {"bottle","rs_test_objects:'KimaxBottle'"},
+    {"bottle_acid","rs_test_objects:'KimaxBottle'"},
+    {"bottle_base","rs_test_objects:'KimaxBottle'"},
+    {"flask_250ml","rs_test_objects:'Flask'"},
+    {"flask_400ml","rs_test_objects:'Flask'"},
+    {"pipette","rs_test_objects:'Pipette'"},
+    {"mixer_ikamag","rs_test_objects::'MixerIkaMag'"}
   };
 
 }
-
-//objects

3b91c6108b5dcc9d98a83456f6759fcf0f5987a3
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2016-12-01 14:54:52 +0100

    paginate

--- rs_web/html/app.py
+++ rs_web/html/app.py
@@ -1,73 +1,117 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template,current_app
+from flask_paginate import Pagination, get_page_args
 
 from pymongo import MongoClient
 
-import bson
-from bson import Binary
-
 import base64
 import numpy as np
-import cv2 
-import cv
+import cv2
 
+from math import ceil
 
 app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+
 #collections = {'rgb:color_imgage_hd','depth:depth_imgage_hd','scene:scene','objects:persistent_objects'}
 
 
-dbName = "Scenes_annotated"
+#dbName = "Scenes_annotated"
 #dbName = "PnP9Obj"
+dbName = "kitchen"
 client = MongoClient()
 db = client[dbName]
 
+
+#@app.before_request
+#def before_request():
+#  g.conn = sqlite3.connect('test.db')
+#  g.conn.row_factory = sqlite3.Row
+#  g.cur = g.conn.cursor()
 
 for collectionName in db.collection_names():
     print collectionName
 
 keyOut=''
-scene_cursor = db.scene.find()
+scene_cursor = db.cas.find()
 timestamps = []
 for sc in scene_cursor:
-    timestamps.append(sc['timestamp'])
+    timestamps.append(sc['_timestamp'])
     for key, value in sc.iteritems():
         if key == 'annotations':
             keyOut=key
 
-print len(timestamps)
+def getBase64Img(img):
+  [ret,png] = cv2.imencode('.png',img) 
+  b64 = base64.b64encode(png.tostring())
+  return 'data:image/png;base64,'+b64
+
+def get_image_for_sceneID(sceneId,scaleFactor):    
+  casDocument = db.cas.find({'_id':sceneId})
+  if casDocument.count() !=0: 
+    colorCursor= db.color_image_hd.find({'_id':casDocument[0]['color_image_hd']})
+    if colorCursor.count()!=0:
+      width = colorCursor[0]['cols']
+      height = colorCursor[0]['rows']            
+      imgData = colorCursor[0]['data']
+      image = np.reshape(np.fromstring(imgData,np.uint8),(height,width,3))
+      small = cv2.resize(image, (0,0), fx=scaleFactor, fy=scaleFactor) 
+      return small
 
 
-def get_image_for_scene(sceneId):    
-    casDocument = db.cas.find({'_id':sceneId})
-    if casDocument.count() !=0: 
-        colorCursor= db.color_image_hd.find({'_id':casDocument[0]['color_image_hd']})
-        if colorCursor.count()!=0:
-            width = colorCursor[0]['cols']
-            height = colorCursor[0]['rows']            
-#            print 'Size of image is: '+str(width)+'x'+str(height)+ '\n'
-#            print 'Type is: ' + str(colorCursor[0]['mat_type'])
-            imgData = colorCursor[0]['data']
+def getImages(timestamps):
+  imgs = []
+  for ts in timestamps:
+    imgs.append(getBase64Img(get_image_for_sceneID(db.scene.find({'timestamp':ts})[0]['_parent'],0.22)))
+  return imgs
 
-            image = np.reshape(np.fromstring(imgData,np.uint8),(960,1280,3))
-            small = cv2.resize(image, (0,0), fx=0.25, fy=0.25) 
-            [ret,jpeg] = cv2.imencode('.png',small)
-            return base64.b64encode(jpeg.tostring())
-            
-#            cv2.imshow('display',nppic)
-#            cv2.waitKey(0)
-#            cv2.destroyAllWindows()
 
-imgs = []
-
-for ts in timestamps:
-   b64 = get_image_for_scene(db.scene.find({'timestamp':ts})[0]['_parent'])
-   imgs.append('data:image/png;base64,'+b64)
+#@app.route('/rs_test.html')
+#def hello(dbname=None,rows=[],images =[]):
+#  imgs = getImages(1,10)
+#  return render_template('bkup.html', dbname=dbName,rows=timestamps,images=imgs)
 
 
 @app.route('/')
-@app.route('/rs_test.html')
-def hello(dbname=None,rows=[],images =[]):
-    return render_template('objStore.html', dbname=dbName,rows=timestamps,images=imgs)
+def index():
+    total = len(timestamps)
+    page, per_page, offset = get_page_args()
+    
+    idxB = (page-1)*per_page
+    idxE = page*per_page  
+    ts = timestamps[idxB:idxE]
+    imgs = getImages(ts)
+    pagination = get_pagination(page=page,
+                                per_page=per_page,
+                                total=total,
+                                record_name='scenes',
+                                format_total=True,
+                                format_number=True,
+                                )
+    return render_template('objStore.html', timestamps=ts,
+                           images=imgs,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
+
+def get_pagination(**kwargs):
+    kwargs.setdefault('record_name', 'records')
+    return Pagination(css_framework=get_css_framework(),
+                      link_size=get_link_size(),
+                      show_single_page=show_single_page_or_not(),
+                      **kwargs
+                      )
+                      
+def get_css_framework():
+    return current_app.config.get('CSS_FRAMEWORK', 'bootstrap3')
+
+
+def get_link_size():
+    return current_app.config.get('LINK_SIZE', 'sm')
+
+
+def show_single_page_or_not():
+    return current_app.config.get('SHOW_SINGLE_PAGE', False)
 
 if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)

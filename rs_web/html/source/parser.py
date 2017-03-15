f2bc27035c84db8a53dd0d90e8c4fd10d4154ae0
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-03-15 15:48:20 +0100

    parser works

--- rs_web/html/source/parser.py
+++ rs_web/html/source/parser.py
@@ -7,36 +7,6 @@ Created on Mon Dec 12 11:19:44 2016
 
 from pyparsing import *
 
-#key = oneOf('location shape color size detection id ts')
-#separator = Literal(',').suppress()
-#lpar = Literal('(').suppress()
-#rpar = Literal(')').suppress()
-#lbr = Literal('[').suppress()
-#rbr = Literal(']').suppress()
-#
-#And = Literal('and').suppress()
-#
-#kvp = lpar + Group(key + separator + Word(alphanums)) + rpar
-#interval = lpar + Group(Word(alphanums) + ':' + Word(alphanums))+rpar
-#
-#objProperty = kvp | interval | Empty()
-#kvps = Forward()
-#atom = objProperty | Group(kvps)
-#kvps << atom + ZeroOrMore(separator + kvps)
-#
-#objDef = Group('object' +lpar + kvps +rpar)
-#objs = Forward()
-#elem = objDef | Group(objs)
-#objs << elem + ZeroOrMore(And+objs)
-#
-#qType = oneOf('scenes views')
-#query1 =  qType + lpar + objs + rpar
-#query2 =  qType + lpar + kvps + rpar
-#
-#query = query1 | query2
-##this sux but for now it should work
-#emptyQuery  = qType+lpar+rpar
-
 #####new parsing: more designator like
 
 bq = Literal('{').suppress()
@@ -44,35 +14,38 @@ eq = Literal('}').suppress()
 bl = Literal('[').suppress()
 el = Literal(']').suppress()
 delimiter = Literal(':').suppress()
-separator = oneOf(', ;')
+separator = oneOf(', ;').suppress()
 
 resultSpecifier = oneOf('scene hypotheses object')
 key = oneOf('location shape color size detection id ts type value confidence') | resultSpecifier
 value = Word(alphanums)
 
-kvp=key+delimiter+value | key+delimiter + description
  
 kvps = Forward() 
-description = bl+ kvps +el
+description = bl+ kvps +el 
 kvp = Group(key+delimiter+value) | Group(key+delimiter+description)
-kvps<<kvp+separator|kvp
+kvps << OneOrMore(kvp+Optional(separator)) |kvps
+
+query = bq+resultSpecifier+delimiter+description+eq
 
 if __name__ == "__main__":  
 
-    s ='object:[shape,box]'
-    results = kvp.parseString(s)
-    print s,'->', results
+    try:    
+        s ='{object:[shape:blue, size:small]}'
+        results = query.parseString(s)
+        print s,'->', results
         
-#        s ='scenes(object((shape,box))'+ \
-#                    'and object((id,2))' + \
-#                  ')'
-#        results = query.parseString( s )
-#        print s,'->', results
-       
-#        s ='views(object((shape,box)) and object((id,2)) and object((id,3)))'
-#        results = query.parseString( s )
-#        print s,'->', results
-    
-#    except:
-#        print('Malformed query. RTFM')
+        s = '{scene:[object:[id:2],object:[id:4]]}'
+        results = query.parseString(s)
+        print s,'->', results    
+        
+        s = '{scene:[hypotheses:[id:2],hypotheses:[color:blue, shape:box, type:crap]]}'
+        results = query.parseString(s)
+        print s,'->', results 
+        
+        
+    except (ParseException):
+        print('Malformed query. RTFM')
+    except:
+        print('Something went horrably wrong!')
         

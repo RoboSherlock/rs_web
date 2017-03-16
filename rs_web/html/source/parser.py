f5ddbd33b926bdbb67a8fa92a60196c60a90f042
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-03-17 00:01:57 +0100

    extended parser

--- rs_web/html/source/parser.py
+++ rs_web/html/source/parser.py
@@ -8,57 +8,83 @@ Created on Mon Dec 12 11:19:44 2016
 from pyparsing import *
 
 #####new parsing: more designator like
-
+class QueryHandler:
+    def __init__(self):
+        print "Init Query Handler"
 #param is token found in string
-def kvp_cb_(toks):
-    print "Token: ",toks
-    #as long as we us add Parse action we don't need to return anything
-    #return toks    
+    def kvp_cb_(self, toks):
+        print "Token: ",toks
+        #as long as we us add Parse action we don't need to return anything
+        return toks    
     
-def res_specif_cp_(toks):
-    #now search DB based on what is queried.
-    print "Result Specifier: ",toks
+    def res_specif_cp_(self,toks):
+        #now search DB based on what is queried.
+        print " : Result Specifier: ",toks
+        return toks
 
+    def descript_cb_(self,toks):
+        print "description of a key: ",toks
+        
+qh = QueryHandler()
 
-    
 bq = Literal('{').suppress()
 eq = Literal('}').suppress()
 bl = Literal('[').suppress()
 el = Literal(']').suppress()
-delimiter = Literal(':').suppress()
+bp = Literal('(').suppress()
+ep = Literal(')').suppress()
+
+delimiter = oneOf(':').suppress()
+operator = oneOf('= < >')
 separator = oneOf(', ;').suppress()
 
-resultSpecifier = oneOf('scene hypotheses object').setParseAction(res_specif_cp_)
-key = oneOf('location shape color size detection id ts type value confidence') | resultSpecifier
-value = Word(alphanums)
+resultSpecifier = oneOf('scene hypotheses object')
+key = oneOf('location shape color size detection id ts type value ratio confidence') | resultSpecifier
 
- 
+point = Literal('.')
+number = Word(nums) 
+plusorminus = Literal('+') | Literal('-')
+integer = Combine( Optional(plusorminus) + number )
+floatnumber = Combine( integer +
+                       Optional( point + Optional(number) )
+                     )
+
 kvps = Forward() 
-description = bl+ kvps +el 
-kvp = Group(key+delimiter+value).addParseAction(kvp_cb_) | Group(key+delimiter+description)
-kvps << OneOrMore(kvp+Optional(separator)) |kvps
+resultDescription = bl+ kvps +el
+description = bp+kvps+ep
 
-query = bq+resultSpecifier+delimiter+description+eq
+value = Word(alphanums) | floatnumber | description.addParseAction(qh.descript_cb_)
+ 
+kvp = Group(key+delimiter+value).addParseAction(qh.kvp_cb_) | Group(key+delimiter+resultDescription)| Group(key+operator+value)
+kvps << (OneOrMore(kvp+Optional(separator)) |kvps)
+
+query = bq+resultSpecifier.addParseAction(qh.res_specif_cp_)+delimiter+resultDescription+eq
 
 if __name__ == "__main__":  
 
     try:    
-        s ='{object:[shape:blue, size:small]}'
-        results = query.parseString(s)
-        print s,'->', results
-        
+#         s ='{object:[shape:blue, size:small]}'
+#         results = query.parseString(s)
+#         print s,'->', results
+#         
         s = '{scene:[object:[id:2],object:[id:4]]}'
         results = query.parseString(s)
         print s,'->', results    
         
-        s = '{scene:[hypotheses:[id:2],hypotheses:[color:blue, shape:box, type:crap]]}'
+        s = '{scene:[hypotheses:[type:Plate,shape:(value:blue, confidence=0)],hypotheses:[color:blue, shape:box, type:crap]]}'
         results = query.parseString(s)
         print s,'->', results 
         
-        s = '{scene:[hypotheses:[id:2],hypotheses:[color:blue, shape:box, type:crap]]}'
-        results = query.parseString(s)
-        print s,'->', results
-        
+#         {scene:[hypotheses:
+#                     [type:Plate],
+#                 hypotheses:
+#                     [color:(value:blue,ratio=0.6) 
+#                      shape:(value:box,confidence>0.5) 
+#                      type:crap,
+#                      classification:[confidence>0.6],
+#                        detection:(source:X,)   
+#                      ]]}
+
     except (ParseException):
         print('Malformed query. RTFM')
     except:

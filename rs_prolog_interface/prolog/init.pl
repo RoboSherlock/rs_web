e306f310097a12070ed3d98cd44ed8976a7cb047
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-11-20 13:54:57 +0100

    register knowrob_map

--- rs_prolog_interface/prolog/init.pl
+++ rs_prolog_interface/prolog/init.pl
@@ -2,6 +2,7 @@
 % dependencies
 :- register_ros_package(knowrob_common).
 :- register_ros_package(knowrob_robosherlock).
+:- register_ros_package(knowrob_map_date).
 :- register_ros_package(rs_prolog_interface).
 
 :- use_module(library(rs_query_interface)).

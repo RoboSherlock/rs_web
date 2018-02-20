cd078ae28a659037ed16d1e4f923674a2abe17aa
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-02-20 14:30:51 +0100

    find all similar objects and wup distances

--- rs_prolog_interface/prolog/rs_similar_objects.pl
+++ rs_prolog_interface/prolog/rs_similar_objects.pl
@@ -1,7 +1,15 @@
 :- module(rs_similar_objects,
   [
-  rs_object_candidates/1
+  rs_object_candidates/1,
+  rdf_all_similar/3
 ]).
+
+
+rdf_all_similar(Class, Super, MostSim) :-
+  findall([A, D], (rdfs_subclass_of(A, Super),
+                   rdf_wup_similarity(A, Class, D)), Dists),
+  predsort(compare_inference_probs, Dists, MostSim).
+
 
 rs_object_candidates([]).
 

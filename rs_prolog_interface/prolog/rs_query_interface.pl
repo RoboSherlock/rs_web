082b1bacd1979e904a7fe0206ee1d525dac884d7
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-04-18 13:51:32 +0200

    this fixes deadlock, it's not nice but it works

--- rs_prolog_interface/prolog/rs_query_interface.pl
+++ rs_prolog_interface/prolog/rs_query_interface.pl
@@ -5,10 +5,8 @@
   rs_pause/1,
   rs_stop/1,
   execute_pipeline/1,
-  scene_clusters_count/3,
   detect_json/1,
   detect/1,
-  query_result/5,
   get_list_of_predicates/2, 
   parse_description/2
 ]).
@@ -136,9 +134,10 @@ designator_type([ A,B | T ] ):-
 detect(List):-
     rs_interface(A),
     parse_description(List,D),
-    cpp_print_desig(D),
+    thread_create((cpp_print_desig(D),
     cpp_process_once(D),
-    cpp_delete_desig(D).
+    cpp_delete_desig(D)),Th,[]).
+     %thread_join(Th,Status).
 
 detect_json(Json):-
     rs_interface(A),
@@ -157,21 +156,5 @@ get_list_of_predicates(Tail,Result).
 
 
 
-%%%%%%%%%%%%%%%% BEGIN: Java Result Queries%%%%%%%%%%%%%%%%%%%%
-%%count object hypotheses logged in a scene by timestamp and Scene name
-scene_clusters_count(Timestamp,Collection,Count):-
-    jpl_new('org.knowrob.robosherlock.db.RSMongoWrapper',[Collection],RS),
-    jpl_call(RS,'getScene',[Timestamp],SceneObject),
-    jpl_call(SceneObject,'getNumberOfClusters',[],Count).		
-
-% Query the results of the database
-% Terms are: shape,color,size...for now..add instance
-% example: query_result(['size','shape'],['medium','flat'],'1409046631131301703','Scenes_annotated',R).
-query_result(Terms,Values,Timestamp,Collection,R):-
-    jpl_new('org.knowrob.robosherlock.db.RSMongoWrapper',[Collection],RS),
-    jpl_call(RS,'getScene',[Timestamp],SceneObject),
-    jpl_list_to_array(Terms,ATerms),
-    jpl_list_to_array(Values,AValues),	
-    jpl_call(SceneObject,'query',[ATerms,AValues],R).	
     
 %%%%%%%%%%%%%%%% END: Java Result Queries%%%%%%%%%%%%%%%%%%%%    

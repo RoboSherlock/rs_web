3a830d6d8de70540e37d630d7cda224712cbf854
HantigC <cristianhantig96@gmail.com>
2018-07-23 14:57:08 +0200

    ADDEN HISTORY IN FLASK

--- rs_web/html/app.py
+++ rs_web/html/app.py
@@ -3,6 +3,7 @@ from __future__ import print_function  # In python 2.7
 from flask import Flask, render_template, current_app, request, jsonify, redirect
 from flask_paginate import Pagination, get_page_args
 
+from gevent.pywsgi import WSGIServer
 import sys
 import re
 import json
@@ -15,11 +16,11 @@ from pyparsing import ParseException
 
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
-
+# http_server = WSGIServer(('', 5555), app)
 
 mc = MongoWrapper()
 qh = QueryHandler()
-
+querys_list = []
 
 @app.route('/', methods=['GET', 'POST'])
 # @app.route('/query', methods=['GET', 'POST'])
@@ -40,6 +41,29 @@ def index():
     return render_template('rs_live.html')
 
 
+@app.route('/robosherlock/add_new_query', methods=['POST'])
+def adding_new_query():
+    query = request.json
+    querys_list.append(query['query'])
+    print(querys_list)
+    return 'OK'
+
+
+@app.route('/robosherlock/get_history_query', methods=['POST'])
+def get_history():
+    data = request.json
+    index = int(data['index'])
+    response = "{\"item\":\""
+    lung_lista = len(querys_list) - 1
+    if index <= -1:
+        index = -1
+    elif index > lung_lista:
+        response = response + querys_list[0]
+        index = lung_lista
+    else:
+        response = response + querys_list[lung_lista - index]
+    response = response + "\",\"index\":" + str(index) + "}"
+    return response
 
 
 @app.route('/prolog_query', methods=['GET', 'POST'])
@@ -165,4 +189,6 @@ def show_single_page_or_not():
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, debug=True, host="0.0.0.0", threaded=True, port=5555)
+    # app.run(use_reloader=True, debug=True, host="0.0.0.0", threaded=True, port=5555)
+    http_server = WSGIServer(('', 5555), app)
+    http_server.serve_forever()

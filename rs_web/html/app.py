8ae53ae45e77c9bc396c41f4c4a96cef2c3d350d
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-08-08 22:39:12 +0200

    cleanup package

--- rs_web/html/app.py
+++ rs_web/html/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from __future__ import print_function  # In python 2.7
 from forms import ScenesForm, HypothesisForm
 from flask import Flask, render_template, current_app, request, jsonify, redirect

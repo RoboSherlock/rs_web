2485d9f3c1a5b419a0585322833c6a75a17d417e
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2020-01-21 17:41:09 +0100

    chmod -x on a bunch of files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=[
-        'rs_wen',
+        'rs_web',
     ],
     package_dir={'': 'html'}
 )
